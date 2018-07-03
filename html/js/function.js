5fdf01a3eebd97ec487f379373157d923f39c790
Chiang <chiangzg@gmail.com>
2018-07-03 16:29:53 +0800

    封装logger

--- html/js/function.js
+++ html/js/function.js
@@ -1,18 +1,62 @@
 //@author Chiang
-function debug(content) {
-    if (DEBUG) {
+
+let logger = {
+    debug: function (content) {
+        if (DEBUG) {
+            let date = (new Date).toLocaleDateString();
+            console.log(date + '[Debug]: ' + content);
+        }
+    },
+    info: function (content) {
         let date = (new Date).toLocaleDateString();
-        console.log(date + '[Debug]: ' + content);
+        console.log(date + '[Info]: ' + content);
+    },
+    error: function (content) {
+        let date = (new Date).toLocaleDateString();
+        console.error(date + '[Error]: ' + content);
     }
+};
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
 }
 
-function info(content) {
-    let date = (new Date).toLocaleDateString();
-    console.log(date + '[Info]: ' + content);
+function existsKey(index, obj) {
+    for (let key in obj) {
+        if (index === key) {
+            return true;
+        }
+    }
+    return false;
 }
 
-function error(content) {
-    let date = (new Date).toLocaleDateString();
-    console.error(date + '[Error]: ' + content);
+/**
+ * 元素对象
+ *
+ * @param id
+ * @constructor
+ */
+function ElementObj(id) {
+    let self = document.getElementById(id);
+    this.html = function (html = null) {
+        if (html == null) {
+            return self.innerHTML;
+        } else {
+            self.innerHTML = html;
+            return null;
+        }
+    };
+    this.setStyle = function (style) {
+        self.setAttribute('style', style);
+    };
+    this.show = function (isShow = true) {
+        if (isShow) {
+            self.style.display = '';
+        } else {
+            self.style.display = 'none';
+        }
+    };
 }
-
